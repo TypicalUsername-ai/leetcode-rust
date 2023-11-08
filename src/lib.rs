b1dd282191797962eb9ac881eaf113a14185f0f2
TypicalUsername-ai <mati.domalewski@gmail.com>
2023-11-08 10:07:23 +0100

    longest_common_prefix (14) solved

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![allow(unused)]
 
 mod add_two_numbers;
+mod longest_common_prefix;
 mod palindrome_number;
 mod seat_reservation_manager;
 mod two_sum;
---
+++ src/longest_common_prefix.rs
@@ -0,0 +1,47 @@
+fn longest_common_prefix(strs: Vec<String>) -> String {
+    if strs.len() == 1 {
+        return strs[0].to_owned();
+    }
+
+    let mut carrs = strs.into_iter();
+    let mut base = carrs.next().unwrap();
+
+    while let Some(cmp) = carrs.next() {
+        let mut new_base = vec![];
+
+        for (c1, c2) in base.chars().zip(cmp.chars()) {
+            if (c1 == c2) {
+                new_base.push(c2)
+            } else {
+                break;
+            }
+        }
+        base = new_base.iter().collect();
+    }
+
+    base
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+
+    #[test]
+    fn case1() {
+        let input = vec![
+            "flower".to_string(),
+            "flow".to_string(),
+            "flight".to_string(),
+        ];
+
+        assert_eq!(longest_common_prefix(input), "fl".to_string());
+    }
+
+    #[test]
+    fn case2() {
+        let input = vec!["dog".to_string(), "racecar".to_string(), "car".to_string()];
+
+        assert_eq!(longest_common_prefix(input), "".to_string());
+    }
+}
