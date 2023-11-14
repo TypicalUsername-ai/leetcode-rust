6ed82046c4045c38b7da0c1043e7cd68f53a197e
TypicalUsername-ai <mati.domalewski@gmail.com>
2023-11-14 09:15:46 +0100

    moved and created structs module for DSs

--- src/add_two_numbers.rs
+++ src/add_two_numbers.rs
@@ -1,16 +1,4 @@
-// Definition for singly-linked list.
-#[derive(PartialEq, Eq, Clone, Debug)]
-pub struct ListNode {
-    pub val: i32,
-    pub next: Option<Box<ListNode>>,
-}
-
-impl ListNode {
-    #[inline]
-    fn new(val: i32) -> Self {
-        ListNode { next: None, val }
-    }
-}
+use crate::structs::singly_linked_list::ListNode;
 
 pub fn add_two_numbers(
     l1: Option<Box<ListNode>>,
---
+++ src/structs/singly_linked_list.rs
@@ -0,0 +1,48 @@
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+
+    pub fn from_vec(v: Vec<i32>) -> Self {
+        let mut iter = v.into_iter();
+        let mut head = Box::new(ListNode::new(iter.next().unwrap()));
+        let mut head_ref = &mut head;
+
+        for val in iter {
+            std::mem::replace(&mut head_ref.next, Some(Box::new(ListNode::new(val))));
+            let next = head_ref.next.as_mut().unwrap();
+            head_ref = next;
+        }
+
+        *head
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn from_vec_test() {
+        let mut testhead = Box::new(ListNode::from_vec(vec![1, 2, 3]));
+
+        assert_eq!(testhead.val, 1);
+
+        testhead = testhead.next.unwrap();
+
+        assert_eq!(testhead.val, 2);
+
+        testhead = testhead.next.unwrap();
+
+        assert_eq!(testhead.val, 3);
+        assert_eq!(testhead.next, None);
+    }
+}
