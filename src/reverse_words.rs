f07cfb5c1ccf2c32421342c19488a6409276b078
TypicalUsername-ai <mati.domalewski@gmail.com>
2024-07-22 15:08:31 +0200

    151: Done

--- src/reverse_words.rs
+++ src/reverse_words.rs
@@ -1,6 +1,15 @@
-pub fn reverse_words(s: String) -> String {}
+pub fn reverse_words(s: String) -> String {
+    s.split_whitespace().rev().collect::<Vec<&str>>().join(" ")
+}
 
 #[cfg(test)]
 mod test {
     use super::*;
+
+    #[test]
+    fn extensive_case() {
+        let s = "  a  good case  ";
+        let ans = "case good a";
+        assert_eq!(reverse_words(s.to_string()), ans)
+    }
 }
